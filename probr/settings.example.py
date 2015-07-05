ddbbc095f5ee147658a261ed739de2c546e76c86
Dominik Schöni <dominik@dominikschoeni.ch>
2015-07-05 16:23:43 +0200

    updated settings-example

--- probr/settings.example.py
+++ probr/settings.example.py
@@ -1,3 +1,10 @@
 from base_settings import *
+import mimetypes
 
-MONGO_URI = 'mongodb://localhost/probr_core'
+BROKER_URL = 'redis://localhost:6379/0'
+MONGO_URI = 'mongodb://localhost/probr_analysis'
+
+# Compress
+COMPRESS_PRECOMPILERS = (
+    ('text/less', 'lessc {infile} {outfile}'),
+)
