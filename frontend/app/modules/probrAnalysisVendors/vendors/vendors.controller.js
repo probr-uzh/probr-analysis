1b66fb5ca0f1dbf35337e23e41480672479eca3f
Dominik Schöni <dominik@dominikschoeni.ch>
2015-07-27 16:25:31 +0200

    fixed loading-spinner

--- frontend/app/modules/probrAnalysisVendors/vendors/vendors.controller.js
+++ frontend/app/modules/probrAnalysisVendors/vendors/vendors.controller.js
@@ -3,16 +3,17 @@
 angular.module('probrAnalysisVendors')
     .controller('VendorsCtrl', function ($scope, $state, $stateParams, $http) {
 
+        $scope.isLoading = true;
         $scope.populationData = undefined;
 
         $http.get('/api/vendors').
             success(function (data, status, headers, config) {
                 var populationData = {'_id': 'Device Vendors', children: data};
                 $scope.populationData = populationData;
+                $scope.isLoading = false;
             }).
             error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
+
             });
 
     });
