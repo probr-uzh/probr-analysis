18aa16889e935be35d22101852f8bbe0bd23cb88
Genc Mazlami <genc.mazlami@uzh.ch>
2015-07-31 00:05:15 +0200

    Include leaflet map (with css visibility problems).

--- frontend/app/modules/probrAnalysisMap/map/map.controller.js
+++ frontend/app/modules/probrAnalysisMap/map/map.controller.js
@@ -18,4 +18,19 @@ angular.module('probrAnalysisMap')
             }
         );
 
+      // initialize the map
+      var map = L.map('map').setView([42.35, -71.08], 13);
+
+      // load a tile layer
+      L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+        {
+          attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+          maxZoom: 17,
+          minZoom: 9
+        }).addTo(map);
+
+
+
+      map.invalidateSize();
+
     });
