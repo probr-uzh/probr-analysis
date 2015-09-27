e1d7867023ad661b31e8bdb29fd234593fa6dfc6
Dominik Schöni <dominik@dominikschoeni.ch>
2015-09-27 22:34:18 +0200

    fixing deployment-skript for data-volume

--- install/docker/deploy.sh
+++ install/docker/deploy.sh
@@ -7,4 +7,4 @@ docker run -d --expose 8080 --name probr_analysis --link probr_mongodb_1:mongodb
 
 docker stop nginx
 docker rm nginx
-docker run -d -p 80:80 --name nginx --volumes-from probr_core --link probr_core:probr_core --link probr_core_ws:probr_core_ws --link probr_analysis:probr_analysis nginx
+docker run -d -p 80:80 --name nginx --volumes-from probr_data --link probr_core:probr_core --link probr_core_ws:probr_core_ws --link probr_analysis:probr_analysis nginx
