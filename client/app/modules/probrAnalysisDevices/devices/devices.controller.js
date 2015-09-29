41775be6ed0e313e54c0b08a1ad96c236fce68c3
Genc Mazlami <genc.mazlami@uzh.ch>
2015-09-30 00:03:39 +0200

    Implement pie chart and top15 ranking for vendors.

--- client/app/modules/probrAnalysisDevices/devices/devices.controller.js
+++ client/app/modules/probrAnalysisDevices/devices/devices.controller.js
@@ -10,14 +10,62 @@ angular.module('probrAnalysisDevices')
     $scope.pageLength = 50;
     $scope.query = {};
     $scope.resource = Device;
-    $scope.filters = {skip: 0, limit: $scope.pageLength};
 
     Device.query({
-        skip: 0,
-        limit: $scope.pageLength
       }, function (resultObj) {
         $scope.devices = resultObj;
+
+        var devices = $scope.devices;
+
+        var vendorMap = new Object();
+
+        var vendorList = [];
+
+        var totalCount = 0;
+
+        devices.forEach(function(element){
+          var vendor = element.vendor;
+          if(vendor !== undefined){
+            totalCount++;
+            if(vendorMap[vendor] === undefined){ //vendor not yet encountered --> add new entry to map
+              vendorMap[vendor] = 1;
+              vendorList.push(vendor);
+            }else{ //vendor already encountered --> increase count for that vendor
+              vendorMap[vendor] = vendorMap[vendor] + 1;
+            }
+          }
+        });
+
+        var data = [];
+
+        var inverseVendorMap = new Object();
+
+        vendorList.forEach(function(element){
+          inverseVendorMap[vendorMap[element]] = element;
+          data.push(vendorMap[element]);
+        });
+
+        //assign data for pie chart
+        $scope.pieLabels = vendorList;
+        $scope.pieData = data;
+
+        //compute data for the ranking table
+        var sortArray = Object.keys(inverseVendorMap);
+
+        //sortArray.sort();
+        sortArray.reverse();
+
+        var ranking = [];
+
+        for(var i=0; i < 15 ; i++){
+          var percent = Number(((sortArray[i] / totalCount) * 100).toFixed(1));
+          ranking.push({rank: i+1, vendor: inverseVendorMap[sortArray[i]] , count: sortArray[i], percentage: (percent + " %")});
+        }
+
+        $scope.ranking = ranking;
+
       }
     );
+
   });
 
