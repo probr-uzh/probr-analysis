471907a4a4c0340c4e62ef66e9924b80fb3b8e5e
Dominik Schöni <dominik@dominikschoeni.ch>
2015-10-12 11:01:13 +0200

    fixes session-job to be truly incremental

--- client/app/modules/probrAnalysisMACAnalyzer/analyzer/analyzer.controller.js
+++ client/app/modules/probrAnalysisMACAnalyzer/analyzer/analyzer.controller.js
@@ -1,32 +1,38 @@
 'use strict';
 
 angular.module('probrAnalysisMACAnalyzer')
-  .controller('AnalyzerCtrl', function ($scope, $state, $stateParams, Socket) {
+    .controller('AnalyzerCtrl', function ($scope, $state, $stateParams, Socket) {
 
-    $scope.packets = [];
-    $scope.watchedAddresses = [];
+        $scope.packets = [];
+        $scope.watchedAddresses = [];
 
-    $scope.addMAC = function() {
-      var address = $scope.macInput;
+        $scope.addMAC = function () {
+            var address = $scope.macInput;
+            address = address.replace(/:/g, "");
 
-      Socket.listenTo('packet:' + address, function (packet) {
-        $scope.packets.push(packet);
-      });
+            if ($scope.watchedAddresses.indexOf(address) !== -1) {
+                $scope.macInput = '';
+                return;
+            }
 
-      $scope.watchedAddresses.push(address);
-      $scope.macInput = '';
+            Socket.listenTo('packet:' + address, function (packet) {
+                $scope.packets.push(packet);
+            });
 
-    }
+            $scope.watchedAddresses.push(address);
+            $scope.macInput = '';
 
-    $scope.removeMAC = function(address) {
-      Socket.unlistenTo('packet:' + address);
+        }
 
-      $scope.packets.splice(_.findIndex($scope.packets, function (packet) {
-        return packet.mac_address_src === address;
-      }), 1);
+        $scope.removeMAC = function (address) {
+            Socket.unlistenTo('packet:' + address);
 
-      $scope.watchedAddresses.splice($scope.watchedAddresses.indexOf(address) , 1);
-    }
+            $scope.packets.splice(_.findIndex($scope.packets, function (packet) {
+                return packet.mac_address_src === address;
+            }), 1);
 
-  });
+            $scope.watchedAddresses.splice($scope.watchedAddresses.indexOf(address), 1);
+        }
+
+    });
 
--- worker/tasks/session.js
+++ worker/tasks/session.js
@@ -32,7 +32,7 @@ module.exports = function (job, done) {
                 console.log("session-Job: incremental map reduce done.");
                 console.log("session-Job: forEach: raw_sessions -> sessions");
 
-                SessionForEach.incremental(0, function () {
+                SessionForEach.incremental(rawSessionBreakTime, function () {
                     console.log("session-Job: forEach: raw_sessions -> sessions -> done");
                     done();
                 });
