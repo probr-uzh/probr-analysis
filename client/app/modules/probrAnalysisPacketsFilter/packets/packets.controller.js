d4756fa9ee142d1632c75b5ffa0de8c8c663b74a
Genc Mazlami <genc.mazlami@uzh.ch>
2015-10-19 18:49:56 +0200

    Implement live/debug view for packets in analysis/packets

--- client/app/modules/probrAnalysisPacketsFilter/packets/packets.controller.js
+++ client/app/modules/probrAnalysisPacketsFilter/packets/packets.controller.js
@@ -3,6 +3,19 @@
 angular.module('probrAnalysisPacketsFilter')
     .controller('PacketsCtrl', function ($scope, $state, $stateParams, Packet, Socket) {
 
+        $scope.archiveHidden = false;
+        $scope.liveHidden = true;
+
+        $scope.toggleArchive = function () {
+          $scope.archiveHidden = false;
+          $scope.liveHidden = true;
+        }
+
+        $scope.toggleLive = function () {
+          $scope.archiveHidden = true;
+          $scope.liveHidden = false;
+        }
+
         $scope.fields = ['mac_address_src: ', 'mac_address_dst: ', 'lat: ', 'lon: ', 'tags: ', 'ssid: ', 'signal_strength: '];
         $scope.newPackets = 0;
         $scope.pageLength = 100;
@@ -18,6 +31,19 @@ angular.module('probrAnalysisPacketsFilter')
         $scope.submit = function (query) {
             $scope.query = query;
         }
+
+        $scope.livePackets = [];
+
+        Socket.listenTo('packet:create', function (packet) {
+          packet.time = new Date(packet.time["$date"]);
+          if($scope.livePackets.length < 20){
+            $scope.livePackets.unshift(packet);
+          }else{
+            $scope.livePackets.pop();
+            $scope.livePackets.unshift(packet);
+
+          }
+        });
 
     });
 
