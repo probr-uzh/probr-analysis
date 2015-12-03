3237bad9c59e235beeb0a013df7565a2792ff0e8
Sebastian Stephan <sstephan1337@gmail.com>
2015-12-03 20:22:25 +0100

    Hotfix. raw_location missing tags.

--- server/api/location/raw_location.model.js
+++ server/api/location/raw_location.model.js
@@ -6,17 +6,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RawLocationSchema = new Schema({
-  value: {
-    numberOfLocations: Number,
-    mac_address: String,
-    time: {type: Date, index: true},
-    locations: [ {
-        long: Number,
-        lat: Number,
-        weightedSignal: Number,
-        cnt: Number
-      } ]
-}});
+    value: {
+        numberOfLocations: Number,
+        mac_address: String,
+        time: {type: Date, index: true},
+        locations: [ {
+            long: Number,
+            lat: Number,
+            weightedSignal: Number,
+            cnt: Number
+        } ],
+        tags: {type: [String]}
+    }});
 
 var RawLocation = mongoose.model('raw_locations',RawLocationSchema,'raw_locations');
 
