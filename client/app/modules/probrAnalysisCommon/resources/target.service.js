c350c193719cce7bc71457f5a7848c96cf77bb20
Genc Mazlami <genc.mazlami@uzh.ch>
2015-12-09 00:06:52 +0100

    Fixes deletion of tracked target persons. Fixes #53.

--- client/app/modules/probrAnalysisCommon/resources/target.service.js
+++ client/app/modules/probrAnalysisCommon/resources/target.service.js
@@ -2,7 +2,7 @@
 
 angular.module('probrAnalysisCommon')
     .factory('Target', function ($resource) {
-        var Target = $resource('api/targets/:targetId/', {packetId: '@_id'},
+        var Target = $resource('api/targets/:targetId/', {targetId: '@_id'},
             {
                 count: {method: 'GET', url: 'api/targets/count', isArray: false},
             }
