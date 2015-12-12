bb1fdc979b8949d2f077084995fe20d1d13e4c92
Sebastian Stephan <sstephan1337@gmail.com>
2015-12-12 20:36:33 +0100

    Remove unnecessairy test tasks. Should fix npm test fail.

--- Gruntfile.js
+++ Gruntfile.js
@@ -625,45 +625,11 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('test', function (target) {
-        if (target === 'server') {
-            return grunt.task.run([
-                'env:all',
-                'env:test',
-                'mochaTest'
-            ]);
-        }
-
-        else if (target === 'client') {
-            return grunt.task.run([
-                'clean:server',
-                'env:all',
-                'injector:less',
-                'concurrent:test',
-                'injector',
-                'autoprefixer',
-                'karma'
-            ]);
-        }
-
-        else if (target === 'e2e') {
-            return grunt.task.run([
-                'clean:server',
-                'env:all',
-                'env:test',
-                'injector:less',
-                'concurrent:test',
-                'injector',
-                'wiredep',
-                'autoprefixer',
-                'express:dev',
-                'protractor'
-            ]);
-        }
-
-        else grunt.task.run([
-                'test:server',
-                'test:client'
-            ]);
+      return grunt.task.run([
+        'env:all',
+        'env:test',
+        'mochaTest'
+      ]);
     });
 
     grunt.registerTask('build', [
