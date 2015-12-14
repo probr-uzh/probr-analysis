0009c16488037ec78c6c205112a7dea785ba3fd4
Dominik Schöni <dominik@dominikschoeni.ch>
2015-12-14 16:14:43 +0100

    fixing base-url replacer

--- install/docker/node/run.sh
+++ install/docker/node/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed -i 's/base href="\/"/base href="\/analysis\/"/g' client/index.html
+sed -i 's/base href="\/"/base href="\/analysis\/"/g' dist/public/index.html
 NODE_ENV=production node dist/server/app.js
